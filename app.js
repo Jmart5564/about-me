db965f99025d674e54c6dadc5c56c9c9e48f89a6
Jmart5564 <Jmart5564@gmail.com>
2022-06-01 17:36:01 -0700

    TOSbutton validated

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-// grab DOM elements
+// grab DOM elements for part 1
+const termsCheckbox = document.getElementById('terms-checkbox');
+const enterButton = document.getElementById('enter-button');
+const creatureSelection = document.getElementById('creature-selection');
+
+// grab DOM elements for part 2
+const nameInput = document.getElementById('name-input');
+const creatureName = document.getElementById('creature-name');
+const imageSelect = document.getElementById('image-select');
+const imageDisplay = document.getElementById('image-display');
 
 // set event listeners
+termsCheckbox.addEventListener('change', () => {
+    enterButton.disabled = !termsCheckbox.checked;
+    console.log("enter button enabled");
+});
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
